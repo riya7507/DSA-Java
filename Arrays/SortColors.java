d2934cde7613b5695e0a4ebcdbc3763dda8b3bb1
riya7507 <riyadeulkar5@gmail.com>
2026-06-13 23:49:04 +0530

    Add SortColors class to sort colors using Dutch Flag
    Implement Dutch National Flag Algorithm to sort colors in an array.

---
+++ Arrays/SortColors.java
@@ -0,0 +1,33 @@
+// LeetCode 75 - Sort Colors
+// Approach: Dutch National Flag Algorithm
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+    public void sortColors(int[] nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+
+                low++;
+                mid++;
+            }
+            else if (nums[mid] == 1) {
+                mid++;
+            }
+            else {
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+
+                high--;
+            }
+        }
+    }
+}
